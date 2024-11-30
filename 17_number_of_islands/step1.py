9aff3b4bb47e2906f46edd951bbb337364fca136
MA_yo_TA <m.yota0906kapa@gmail.com>
2024-11-30 22:51:32 +0900

    2024-11-30 Number of Islands

---
+++ 17_number_of_islands/step1.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.mark_connected_island(grid, i, j)
+                    count += 1
+
+        return count
+
+    def mark_connected_island(self, grid, i, j):
+        if i < 0 or len(grid) <= i or j < 0 or len(grid[0]) <= j or grid[i][j] != "1":
+            return
+
+        grid[i][j] = "#"
+        self.mark_connected_island(grid, i - 1, j)
+        self.mark_connected_island(grid, i + 1, j)
+        self.mark_connected_island(grid, i, j - 1)
+        self.mark_connected_island(grid, i, j + 1)
